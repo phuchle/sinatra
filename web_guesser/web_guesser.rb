cf350915de4d9d34b0da5a88341839da3e2883ec
Phuc Le <le.phuc.h@gmail.com>
2016-12-14 11:32:48 -0800

    Add color changing background depending on guesses made

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -4,14 +4,21 @@ require 'sinatra/reloader'
 set :secret_number, rand(100)
 
 get '/' do
-  guess = params["guess"].to_i
+  guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => {:number => settings.secret_number, :message => message}
+  background_color = set_background_color(message)
+
+  erb :index, :locals => {
+    :number => settings.secret_number,
+    :message => message,
+    :background_color => background_color
+  }
 end
 
 def check_guess(guess)
-  guess = guess
-
+  return nil if guess.nil?
+  
+  guess = guess.to_i
   if (guess - settings.secret_number) > 5
     "Way too high!"
   elsif settings.secret_number - guess > 5
@@ -22,5 +29,17 @@ def check_guess(guess)
     "Too low!"
   elsif guess == settings.secret_number
     "You got it right!"
+  end
+end
+
+def set_background_color(message)
+  if message == "Way too high!" || message == "Way too low!"
+    "red"
+  elsif message == "Too high!" || message == "Too low!"
+    "lightpink"
+  elsif message == "You got it right!"
+    "green"
+  elsif message.nil?
+    "white"
   end
 end
