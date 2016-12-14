8cc56b2ccde98bdf46a96a398324316bb96de474
Phuc Le <le.phuc.h@gmail.com>
2016-12-14 13:08:00 -0800

    Limit number of guesses that can be made before winning or losing

--- hi.rb
+++ hi.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 
+number = rand(100)
+
 get '/' do
     "Hello World!"
 end
--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 set :secret_number, rand(100)
+@@guess_remaining = 5
 
 get '/' do
   guess = params["guess"]
@@ -11,15 +12,29 @@ get '/' do
   erb :index, :locals => {
     :number => settings.secret_number,
     :message => message,
-    :background_color => background_color
+    :background_color => background_color,
+    :guess_remaining => @@guess_remaining
   }
+end
+
+def make_secret_number
+  settings.secret_number = rand(100)
+  @@guess_remaining = 5
 end
 
 def check_guess(guess)
   return nil if guess.nil?
-  
+
+  @@guess_remaining -= 1
   guess = guess.to_i
-  if (guess - settings.secret_number) > 5
+
+  if @@guess_remaining == 0
+    make_secret_number
+    "You've lost but a new number has been created.  Try again!"
+  elsif guess == settings.secret_number
+    make_secret_number
+    "You got it right! A new number has been set."
+  elsif (guess - settings.secret_number) > 5
     "Way too high!"
   elsif settings.secret_number - guess > 5
     "Way too low!"
@@ -27,8 +42,6 @@ def check_guess(guess)
     "Too high!"
   elsif guess < settings.secret_number
     "Too low!"
-  elsif guess == settings.secret_number
-    "You got it right!"
   end
 end
 
@@ -37,7 +50,7 @@ def set_background_color(message)
     "red"
   elsif message == "Too high!" || message == "Too low!"
     "lightpink"
-  elsif message == "You got it right!"
+  elsif message == "You got it right! A new number has been set."
     "green"
   elsif message.nil?
     "white"
